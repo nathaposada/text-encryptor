91143807b25adcfa8ffac4f1e65c20d8c08db8be
nathaposada <131016331+nathaposada@users.noreply.github.com>
2023-05-02 11:59:07 -0500

    cambio de nombre de imagen, agregar ids en html y creación de la funcionalidad de javascript

--- encriptador.js
+++ encriptador.js
@@ -1,11 +1,42 @@
-function llaves() {
-   
-    var e = "enter"
-    var i = "imes"
-    var a = "ai"
-    var o = "ober"
-    var u = "ufat"
+function encriptar(){
+  let texto = document.getElementById("texto").value;
+  let tituloMensaje = document.getElementById("titulo-mensaje");
+  let parrafo = document.getElementById("parrafo");
+  let panda = document.getElementById("panda");
+
+  let textoCifrado = texto
+    .replace(/e/gi, "enter")
+    .replace(/i/gi, "imes")
+    .replace(/a/gi, "ai")
+    .replace(/o/gi, "ober")
+    .replace(/u/gi, "ufat");
+
+    if(texto.length != 0 ){
+       document.getElementById("texto").value = textoCifrado;
+       tituloMensaje.textContent = "Texto encriptado con exito";
+       parrafo.textContent = "";
+       panda.src = "./img/oso-panda-v2.png";
+    } 
     
+    else{
+      panda.src = "./img/oso-panda-v2.png";
+      tituloMensaje.textContent = "Ningún mensaje fue encontrado"
+      parrafo.textContent = "Ingresa el texto que desees encriptar o desencriptar";
+      alert("Debes ingresar algún texto");
+    }
+
+
 }
+
+
+//function llaves() {
+   
+  //  var e = "enter"
+  //  var i = "imes"
+  //  var a = "ai"
+  //  var o = "ober"
+  //  var u = "ufat"
+    
+//}
 
 
