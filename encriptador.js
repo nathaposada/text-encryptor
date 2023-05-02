0ece4c5674f89c40d282ee205f6d3f8227c5e857
nathaposada <131016331+nathaposada@users.noreply.github.com>
2023-05-02 12:44:30 -0500

    agregar la imagen del favicon y darle la funcionalidad de desencriptar

--- encriptador.js
+++ encriptador.js
@@ -2,7 +2,6 @@ function encriptar(){
   let texto = document.getElementById("texto").value;
   let tituloMensaje = document.getElementById("titulo-mensaje");
   let parrafo = document.getElementById("parrafo");
-  let panda = document.getElementById("panda");
 
   let textoCifrado = texto
     .replace(/e/gi, "enter")
@@ -15,11 +14,10 @@ function encriptar(){
        document.getElementById("texto").value = textoCifrado;
        tituloMensaje.textContent = "Texto encriptado con exito";
        parrafo.textContent = "";
-       panda.src = "./img/oso-panda-v2.png";
+  
     } 
     
     else{
-      panda.src = "./img/oso-panda-v2.png";
       tituloMensaje.textContent = "Ningún mensaje fue encontrado"
       parrafo.textContent = "Ingresa el texto que desees encriptar o desencriptar";
       alert("Debes ingresar algún texto");
@@ -28,15 +26,43 @@ function encriptar(){
 
 }
 
+function desencriptar() {
+  let texto = document.getElementById("texto").value;
+  let tituloMensaje = document.getElementById("titulo-mensaje");
+  let parrafo = document.getElementById("parrafo");
 
-//function llaves() {
-   
-  //  var e = "enter"
-  //  var i = "imes"
-  //  var a = "ai"
-  //  var o = "ober"
-  //  var u = "ufat"
+  let textoCifrado = texto
+    .replace( /enter/gi, "e")
+    .replace(/imes/gi, "i")
+    .replace(/ai/gi, "a")
+    .replace(/ober/gi, "o")
+    .replace(/ufat/gi, "u");
+
+    if(texto.length != 0 ){
+      document.getElementById("texto").value = textoCifrado;
+      tituloMensaje.textContent = "Texto desencriptado con exito";
+      parrafo.textContent = "";
     
-//}
+    }  
+
+    else {
+
+      tituloMensaje.textContent = "Ningún mensaje fue encontrado"
+      parrafo.textContent = "Ingresa el texto que desees encriptar o desencriptar";
+      alert("Debes ingresar algún texto");
+
+    }
+}
+
+
+//requisitos
+   
+  //   e = "enter"
+  //   i = "imes"
+  //   a = "ai"
+  //   o = "ober"
+  //   u = "ufat"
+    
+
 
 
